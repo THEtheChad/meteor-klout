d5fca8377028a847b04dda965b5b151c9552a668
THEtheChad <chad.d.elliott@gmail.com>
2013-11-11 03:24:21 -0600

    attempting to load package

--- package.js
+++ package.js
@@ -3,8 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
-  api.export && api.export('Klout');
-
+  api.export('Klout');
   api.use('http', 'server');
   api.add_files('lib/klout.js', 'server');
 });
