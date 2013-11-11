2f62650ca6fcaf9bcac75071d52ffb91e06283bc
THEtheChad <chad.d.elliott@gmail.com>
2013-11-11 03:35:52 -0600

    trying to get package to load

---
+++ klout.js
@@ -0,0 +1,53 @@
+/*
+ * klout
+ * https://github.com/THEtheChad/klout
+ *
+ * Copyright (c) 2013 THEtheChad
+ * Licensed under the MIT license.
+ */
+var PROTOCOL = "http";
+var BASE = "api.klout.com";
+var VERSION = "v2";
+var ROOT = PROTOCOL + '://' + BASE + '/' + VERSION + '/';
+
+console.log('Loading Klout...');
+
+function Klout(config){
+	var self = this;
+	self.key = config.key;
+}
+
+Klout.prototype = {
+
+	user: function(id, end_point, cb){
+		return HTTP.get(ROOT + 'user.json/' + id + '/' + end_point + '?key=' + this.key, cb);
+	},
+
+	identity: function(id, end_point, cb){
+		return HTTP.get(ROOT + 'identity.json/' + id, cb);
+	},
+
+	score: function(id, cb){
+		return this.user(id, 'score', cb);
+	},
+
+	influence: function(id, cb){
+		return this.user(id, 'influence', cb);
+	},
+
+	topics: function(ids, cb){
+		return this.user(id, 'topics', cb);
+	},
+
+	getId: function(id, cb){
+		var id_num = +id;
+
+		// id is not a number (ie screenName)
+		if(id_num != id_num){
+			return this.identity(id, 'twitter?screenName=', cb);
+		}
+		else{
+			return this.identity(id, 'tw/', cb);
+		}
+	}
+};
--- package.js
+++ package.js
@@ -2,8 +2,8 @@ Package.describe({
   summary: "Klout API service for Meteor"
 });
 
-Package.on_use(function(api) {
+Package.on_use(function(api){
   api.export('Klout');
   api.use('http', 'server');
-  api.add_files('lib/klout.js', 'server');
+  api.add_files('klout.js', 'server');
 });
